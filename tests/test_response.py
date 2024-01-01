f0ee88c6230d935781a594b95fdb07dd2863a6ea
Kyle Shaver <kyle.j.shaver@gmail.com>
2023-12-31 21:50:28 -0800

    classify Response tests

--- tests/test_response.py
+++ tests/test_response.py
@@ -4,80 +4,99 @@ from tests.utils import double_number
 from tpify import Response, StatusResponse, tpify
 
 
-def test_httpized_response():
-    def double_number_httpized_response(n: int):
-        start = perf_counter()
-        resp = double_number(n)
-        return Response(
-            double_number_httpized_response,
-            (n,),
-            None,
-            200,
-            resp,
-            {},
-            perf_counter() - start,
-        )
+class TestCoreFunction:
+    def test_tpified_response(_):
+        def double_number_tpified_response(n: int):
+            start = perf_counter()
+            resp = double_number(n)
+            return Response(
+                double_number_tpified_response,
+                (n,),
+                None,
+                200,
+                resp,
+                {},
+                perf_counter() - start,
+            )
 
-    arg_val = (2,)
-    resp = double_number_httpized_response(*arg_val)
-    assert isinstance(resp, Response)
-    assert resp.content == double_number(*arg_val)
-    assert resp.status_code == 200
+        arg_val = (2,)
+        resp = double_number_tpified_response(*arg_val)
+        assert isinstance(resp, Response)
+        assert resp.content == double_number(*arg_val)
+        assert resp.status_code == 200
+        assert str(resp) == "<Response [200]>"
+
+    def test_tpified_status_response(_):
+        def doble_number_tpified_status_response(n: int):
+            return StatusResponse(200, double_number(n))
+
+        arg_val = (2,)
+        resp = doble_number_tpified_status_response(*arg_val)
+        assert isinstance(resp, StatusResponse)
+        assert resp.content == double_number(*arg_val)
+        assert resp.status_code == 200
+        assert str(resp) == "<StatusResponse [200]>"
+
+    def test_double_number_decorator(_):
+        @tpify
+        def double_number_decorator(n: int):
+            return double_number(n)
+
+        arg_val = (2,)
+        resp = double_number_decorator(*arg_val)
+        assert isinstance(resp, Response)
+        assert resp.content == double_number(*arg_val)
+        assert resp.status_code == 200
+
+    def test_double_number_tpified_response_decorator(_):
+        @tpify
+        def double_number_tpified_response_decorator(n: int):
+            start = perf_counter()
+            resp = double_number(n)
+            return Response(
+                double_number_tpified_response_decorator,
+                (n,),
+                None,
+                200,
+                resp,
+                {},
+                perf_counter() - start,
+            )
+
+        arg_val = (2,)
+        resp = double_number_tpified_response_decorator(*arg_val)
+        assert isinstance(resp, Response)
+        assert resp.content == double_number(*arg_val)
+        assert resp.status_code == 200
+
+    def test_doble_number_tpified_status_response_decorator(_):
+        @tpify
+        def doble_number_tpified_status_response_decorator(n: int):
+            return StatusResponse(200, double_number(n))
+
+        arg_val = (2,)
+        resp = doble_number_tpified_status_response_decorator(*arg_val)
+        assert isinstance(resp, Response)
+        assert resp.content == double_number(*arg_val)
+        assert resp.status_code == 200
+        print(resp.func.__name__)
 
 
-def test_httpized_status_response():
-    def doble_number_httpized_status_response(n: int):
-        return StatusResponse(200, double_number(n))
+class TestExceptions:
+    def test_raise_exception_default_500(_):
+        @tpify
+        def raise_exception():
+            raise Exception("This could be any exception in a function")
 
-    arg_val = (2,)
-    resp = doble_number_httpized_status_response(*arg_val)
-    assert isinstance(resp, StatusResponse)
-    assert resp.content == double_number(*arg_val)
-    assert resp.status_code == 200
+        resp = raise_exception()
+        assert resp.status_code == 500
+        assert isinstance(resp.content, Exception)
 
+    def test_raise_exception_status_response(_):
+        @tpify
+        def raise_exception():
+            return StatusResponse(403, ValueError("You're not allowed to do that"))
 
-def test_double_number_decorator():
-    @tpify
-    def double_number_decorator(n: int):
-        return double_number(n)
-
-    arg_val = (2,)
-    resp = double_number_decorator(*arg_val)
-    assert isinstance(resp, Response)
-    assert resp.content == double_number(*arg_val)
-    assert resp.status_code == 200
-
-
-def test_double_number_httpized_response_decorator():
-    @tpify
-    def double_number_httpized_response_decorator(n: int):
-        start = perf_counter()
-        resp = double_number(n)
-        return Response(
-            double_number_httpized_response_decorator,
-            (n,),
-            None,
-            200,
-            resp,
-            {},
-            perf_counter() - start,
-        )
-
-    arg_val = (2,)
-    resp = double_number_httpized_response_decorator(*arg_val)
-    assert isinstance(resp, Response)
-    assert resp.content == double_number(*arg_val)
-    assert resp.status_code == 200
-
-
-def test_doble_number_httpized_status_response_decorator():
-    @tpify
-    def doble_number_httpized_status_response_decorator(n: int):
-        return StatusResponse(200, double_number(n))
-
-    arg_val = (2,)
-    resp = doble_number_httpized_status_response_decorator(*arg_val)
-    assert isinstance(resp, Response)
-    assert resp.content == double_number(*arg_val)
-    assert resp.status_code == 200
-    print(resp.func.__name__)
+        resp = raise_exception()
+        assert resp.status_code == 403
+        assert isinstance(resp.content, ValueError)
